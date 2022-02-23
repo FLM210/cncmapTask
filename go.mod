260b9daef2236911c013852bde59cba76406e0f5
二两螺蛳粉儿 <zflmiy@qq.com>
2022-02-23 17:59:58 +0800

    优雅停止

--- go.mod
+++ go.mod
@@ -4,4 +4,7 @@ go 1.17
 
 require github.com/gorilla/handlers v1.5.1
 
-require github.com/felixge/httpsnoop v1.0.1 // indirect
+require (
+	github.com/Unknwon/goconfig v1.0.0 // indirect
+	github.com/felixge/httpsnoop v1.0.1 // indirect
+)
--- gracefulTermination.go
+++ gracefulTermination.go
@@ -3,6 +3,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -10,35 +11,63 @@ import (
 	"os/signal"
 	"syscall"
 	"time"
+
+	"github.com/Unknwon/goconfig"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello World, %v\n", time.Now())
+	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
+		responseHeader(rw, r)
+		fmt.Fprintf(rw, "Hello World, %v\n", time.Now())
+	})
+	http.HandleFunc("/healthz", func(rw http.ResponseWriter, r *http.Request) {
+		responseHeader(rw, r)
+		fmt.Fprint(rw, "server is healthy")
 	})
 
+	listenPort := os.Getenv("listenPort")
+	if listenPort != "" {
+		fmt.Print("user env listenPort")
+		listenPort = ":" + listenPort
+	} else {
+		cfg, err := goconfig.LoadConfigFile("http.conf")
+		if err == nil {
+			fmt.Println("use conf")
+			confPort, _ := cfg.GetValue("", "listenPort")
+			listenPort = ":" + confPort
+		} else {
+			fmt.Print("use dafault 80")
+			listenPort = ":80"
+		}
+	}
+
+	fmt.Println(listenPort)
 	s := &http.Server{
-		Addr:           ":8080",
+		Addr:           listenPort,
 		Handler:        http.DefaultServeMux,
 		ReadTimeout:    10 * time.Second,
 		WriteTimeout:   10 * time.Second,
 		MaxHeaderBytes: 1 << 20,
 	}
-
 	go func() {
 		log.Println(s.ListenAndServe())
 		log.Println("server shutdown")
 	}()
 
 	// Handle SIGINT and SIGTERM.
-	ch := make(chan os.Signal)
+	ch := make(chan os.Signal, 1)
 	signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
 	log.Println(<-ch)
-
 	// Stop the service gracefully.
 	log.Println(s.Shutdown(nil))
-
 	// Wait gorotine print shutdown message
-	time.Sleep(time.Second * 5)
-	log.Println("done.")
+}
+
+//write request header to reponse header
+func responseHeader(w http.ResponseWriter, r *http.Request) {
+	for i, m := range r.Header {
+		reposeVaule, _ := json.Marshal(m)
+		w.Header().Add(i, string(reposeVaule))
+	}
+	w.Header().Add("VERSION", os.Getenv("VERSION"))
 }
